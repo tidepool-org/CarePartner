ef27204898b752d6fb9fcf338816924ddd0cec6c
Pete Schwamb <pete@schwamb.net>
2023-02-21 17:21:03 -0600

    Initial Commit

---
+++ CarePartner/CarePartnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarePartnerApp.swift
+//  CarePartner
+//
+//  Created by Pete Schwamb on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CarePartnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
